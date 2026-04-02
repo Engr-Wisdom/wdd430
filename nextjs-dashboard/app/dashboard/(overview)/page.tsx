0cb28551930bfb6555ccc1018e7d3e87f01cabe4
Wisdom Effiong <wisdomeffiong2008@gmail.com>
2026-04-02 13:40:51 +0100

    Week 3 Assignment

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -5,8 +5,6 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
-
-export const dynamic = 'force-dynamic';
  
 export default async function Page() {
   return (
